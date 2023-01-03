7a4dff6ed18d42197018384c2b62f0885bdd1b31
Mike <52970344+Developer-Mike@users.noreply.github.com>
2023-01-03 11:11:51 +0100

    First Commit

---
+++ src/main/java/mike/autototem/client/AutototemClient.java
@@ -0,0 +1,11 @@
+package mike.autototem.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class AutototemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
